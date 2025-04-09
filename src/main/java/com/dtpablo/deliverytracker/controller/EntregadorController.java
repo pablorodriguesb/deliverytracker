a38815b89f737fad5643e4d10229d9f152317cf0
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-04-09 02:53:02 -0300

    Implementando endpoist para listar todas as rotas

--- src/main/java/com/dtpablo/deliverytracker/controller/EntregadorController.java
+++ src/main/java/com/dtpablo/deliverytracker/controller/EntregadorController.java
@@ -1,27 +1,33 @@
 package com.dtpablo.deliverytracker.controller;
 
-import com.dtpablo.deliverytracker.entity.Entregador;
-import com.dtpablo.deliverytracker.repository.EntregadorRepository;
+import com.dtpablo.deliverytracker.entity.Rota;
+import com.dtpablo.deliverytracker.repository.RotaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/entregadores")
+@RequestMapping("/api/rotas")
 @RequiredArgsConstructor
-public class EntregadorController {
+public class RotaController {
 
-    private final EntregadorRepository entregadorRepository;
-
-    @GetMapping
-    public List<Entregador> listar() {
-        return entregadorRepository.findAll();
-    }
+    private final RotaRepository rotaRepository;
 
     @PostMapping
-    public Entregador criar(@RequestBody Entregador entregador) {
-        return entregadorRepository.save(entregador);
+    public Rota criar(@RequestBody Rota rota) {
+        return rotaRepository.save(rota);
+    }
+
+    //  NOVO: Listar todas as rotas
+    @GetMapping
+    public List<Rota> listarTodas() {
+        return rotaRepository.findAll();
+    }
+
+    // ? Rota específica por entregador
+    @GetMapping("/{entregadorId}")
+    public List<Rota> listarPorEntregador(@PathVariable Long entregadorId) {
+        return rotaRepository.findByEntregadorId(entregadorId);
     }
 }
-
