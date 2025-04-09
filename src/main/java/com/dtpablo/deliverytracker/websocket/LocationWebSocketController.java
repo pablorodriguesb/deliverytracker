35873c966da62afd1d87bdc5f54c3a41e27326e2
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-04-09 01:35:47 -0300

    Configuracao e controladores WebSocket para rastreamento em tempo real

---
+++ src/main/java/com/dtpablo/deliverytracker/websocket/LocationWebSocketController.java
@@ -0,0 +1,28 @@
+package com.dtpablo.deliverytracker.websocket;
+
+import com.dtpablo.deliverytracker.entity.Entregador;
+import com.dtpablo.deliverytracker.repository.EntregadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LocationWebSocketController {
+
+    @Autowired
+    private EntregadorRepository entregadorRepository;
+
+    @MessageMapping("/location")
+    @SendTo("/topic/updates")
+    public LocationMessage updateLocation(LocationMessage message) {
+        Entregador entregador = entregadorRepository.findById(message.getEntregadorId())
+                .orElseThrow(() -> new RuntimeException("Entregador não encontrado"));
+
+        entregador.setLatitude(message.getLatitude());
+        entregador.setLongitude(message.getLongitude());
+        entregadorRepository.save(entregador);
+
+        return message; // Envia de volta para todos os clientes
+    }
+}
