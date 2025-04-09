be2978358b481937ce7ce2c13d71ea7066f8c286
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-04-09 01:20:23 -0300

    Criacao do projeto com Spring Initializr e dependencias

---
+++ src/test/java/com/dtpablo/deliverytracker/DeliverytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtpablo.deliverytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
